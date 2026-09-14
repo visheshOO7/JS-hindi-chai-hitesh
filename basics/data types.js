140901f27183ce260795506316a658fabf66f5fe
Vishesh <215891998+visheshOO7@users.noreply.github.com>
2026-09-14 20:00:25 +0530

    num-and-math

--- basics/data types.js
+++ basics/data types.js
@@ -1,3 +1,8 @@
-console.log("Vishesh");
-console.log("hello");
 //null is of Object data type
+
+// const num = 123.96
+// console.log(num.toPrecision(3));
+
+const min = 10;
+const max = 20;
+console. log(Math.floor(Math. random() * (max - min + 1)) + min);
